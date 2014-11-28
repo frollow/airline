d67ed39c6d8f0ed2adb523a7f79833cb78f622f4
abakhtin <vmanchenko@wooppay.com>
2014-11-28 16:51:42 +0600

    Heroku migration preparations

--- airline/settings.py
+++ airline/settings.py
@@ -77,6 +77,11 @@ WSGI_APPLICATION = 'airline.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": "dp5c00gno3a2j",
+        "USER": "gwlndioundmjln",
+        "PASSWORD": "Xa43phk7U6D3CfPFI0bxNtVipN",
+        "HOST": "ec2-54-225-182-133.compute-1.amazonaws.com",
+        "PORT": "",
     }
 }
 
