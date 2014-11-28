a95522c72943341d8c8613dbdbbd8c871b3d14b5
abakhtin <vmanchenko@wooppay.com>
2014-11-28 21:04:26 +0600

    Heroku magic

--- airline/settings.py
+++ airline/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         "NAME": "avia",
         "USER": "avia",
-        "PASSWORD": "123456",
+        "PASSWORD": "12345",
         "HOST": "localhost",
         "PORT": "",
     }
